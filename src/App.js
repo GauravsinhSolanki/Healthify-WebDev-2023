a5aeb33912040205537a6499a2c944fac2574e41
TejaSreeA <tj508480@dal.ca>
2023-06-20 21:52:18 -0300

    Home page added

--- src/App.js
+++ src/App.js
@@ -9,15 +9,21 @@ import "./App.css";
 import Registration from "./Registration";
 import Profile from "./Profile";
 import FAQPage from "./FAQ";
+import Home from "./Home";
+import Navbar from "./Components/Navbar";
+import Blog from './Blog';
+import './style.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Navbar/>
         <Routes>  
-          <Route path="/" element={<Navigate to="/faq" />} />
+          <Route path="/" element={<Home/>} />
           <Route path="/faq" element={<FAQPage />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/blog" element={<Blog/>}/>
         </Routes>
       </div>
     </Router>
---
+++ src/Single.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Single = () => {
+  return (
+    <div className='single'>
+      <div className='content'>
+      <div className='user'>
+      <div className='info'>
+        <span>Dr. John</span>
+        <p>Posted 2 days ago</p>
+      </div>
+      </div>
+
+      <h1>The Vitality of Eating Healthy: Nurturing Your Body and Mind</h1>
+      <p>
+      Welcome to my blog, where we delve into the significance of maintaining a healthy diet and the profound impact it has on our overall well-being. As a medical professional, I have witnessed firsthand the transformative effects of proper nutrition on countless patients. Join me as we explore the remarkable benefits that come with embracing a healthy eating lifestyle.
+      <br/>
+      <br/>
+      Imagine your body as a well-oiled machine, tirelessly performing its functions day in and day out. Just like any machine, it requires the right kind of fuel to operate efficiently. Proper nutrition provides the necessary nutrients, vitamins, and minerals that our bodies need to function optimally. By consuming a balanced diet, rich in fruits, vegetables, lean proteins, whole grains, and healthy fats, we provide our bodies with the energy and building blocks they need to thrive.
+      <br/>
+      <br/>
+      A robust immune system is crucial for warding off illnesses and maintaining good health. Eating a variety of nutrient-dense foods strengthens our immune response and reduces the risk of infections. Foods such as citrus fruits, leafy greens, garlic, and yogurt are known to enhance immunity. By incorporating these foods into our daily diet, we fortify our defenses, enabling our bodies to fight off diseases more effectively.
+      <br/>
+      <br/>
+      Maintaining a healthy weight is not solely about appearance; it is also about promoting overall wellness. Consuming a balanced diet, coupled with regular physical activity, helps us achieve and sustain an ideal weight. Excessive weight gain increases the risk of various health conditions, including heart disease, diabetes, and certain cancers. By embracing a healthy eating lifestyle, we reduce the likelihood of developing these ailments and improve our quality of life.
+      <br/>
+      <br/>
+      Did you know that what we eat can impact our mental well-being? Studies have shown that a nutrient-rich diet can positively influence our mood, cognitive function, and mental health. Foods such as fatty fish (rich in omega-3 fatty acids), nuts, seeds, whole grains, and fruits contain nutrients that support brain health. Conversely, a diet high in processed foods, saturated fats, and sugars has been linked to an increased risk of depression and anxiety. By prioritizing nutritious foods, we nourish not only our bodies but also our minds.
+      <br/>
+      <br/>
+      The old adage "prevention is better than cure" holds true when it comes to chronic diseases. Unhealthy eating habits are strongly associated with conditions such as obesity, type 2 diabetes, hypertension, and cardiovascular diseases. By adopting a healthy diet, we significantly reduce the risk of developing these ailments. Consuming a variety of whole foods, while limiting processed and sugary foods, ensures that we receive the essential nutrients that support our body's natural defense mechanisms
+      <br/>
+      <br/>
+      </p>
+      </div>
+      <div className='menu'></div>
+    </div>
+
+  )
+}
+
+export default Single
