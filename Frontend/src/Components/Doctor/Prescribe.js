3e4727a5408a10e643be6ae928e89302282fa70b
GauravsinhSolanki <solankigbs11@gmail.com>
2023-08-14 10:20:54 -0300

    Navbar Updated

--- Frontend/src/Components/Doctor/Prescribe.js
+++ Frontend/src/Components/Doctor/Prescribe.js
@@ -53,60 +53,82 @@ const Prescribe = () => {
     };
 
     return (
+      <>
+          <Navbar />
         <div className="container">
-            <Navbar />
-            <h1 className="text-center mt-4">Prescription</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="pid" className="form-label">Patient ID</label>
-                    <select
-                        className="form-control"
-                        id="pid"
-                        name="pid"
-                        value={prescription.pid}
-                        onChange={handleChange}
-                        required
-                    >
-                        <option value="" disabled>Select a patient</option>
-                        {patients.map((patient, index) => (
-                            <option key={index} value={patient._id}>{patient.firstName + " " + patient.lastName}</option>
-                        ))}
-                    </select>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="prescription" className="form-label">Prescription</label>
-                    <textarea
-                        className="form-control"
-                        id="prescription"
-                        name="prescription"
-                        value={prescription.prescription}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="expiryDate" className="form-label">Expiry Date</label>
-                    <input type="date" className="form-control" id="expiryDate" name="expiryDate" value={prescription.expiryDate} onChange={handleChange} required />
-                </div>
-                <button type="submit" className="btn btn-primary">Submit Prescription</button>
-            </form>
-            <Button
-                variant="contained"
-                color="primary"
-                style={{ margin: '10px' }}
-                onClick={() => navigate('/doctor/prescriptions')}
-            >
-                View Prescriptions
-            </Button>
-            <Button
-                variant="outlined"
-                color="secondary"
-                style={{ margin: '10px' }}
-                onClick={() => navigate('/')}
-            >
-                Back
-            </Button>
+          <h1 className="text-center mt-4">Prescription</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="pid" className="form-label">
+                Patient ID
+              </label>
+              <select
+                className="form-control"
+                id="pid"
+                name="pid"
+                value={prescription.pid}
+                onChange={handleChange}
+                required
+              >
+                <option value="" disabled>
+                  Select a patient
+                </option>
+                {patients.map((patient, index) => (
+                  <option key={index} value={patient._id}>
+                    {patient.firstName + " " + patient.lastName}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="prescription" className="form-label">
+                Prescription
+              </label>
+              <textarea
+                className="form-control"
+                id="prescription"
+                name="prescription"
+                value={prescription.prescription}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="expiryDate" className="form-label">
+                Expiry Date
+              </label>
+              <input
+                type="date"
+                className="form-control"
+                id="expiryDate"
+                name="expiryDate"
+                value={prescription.expiryDate}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit Prescription
+            </button>
+          </form>
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ margin: "10px" }}
+            onClick={() => navigate("/doctor/prescriptions")}
+          >
+            View Prescriptions
+          </Button>
+          <Button
+            variant="outlined"
+            color="secondary"
+            style={{ margin: "10px" }}
+            onClick={() => navigate("/")}
+          >
+            Back
+          </Button>
         </div>
+      </>
     );
 };
 
